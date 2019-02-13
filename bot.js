2d4301ec378df3c87197f14096945b127169f516
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-13 18:41:51 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,40 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "!"
+
+client.on('ready', () => {
+console.log(`سجل الدخول ${client.user.tag}!`);
+console.log('')
+console.log('')
+console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+console.log(`[Start] ${new Date()}`);
+console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+console.log('')
+console.log('╔[════════════════════════════════════]╗');
+console.log(`تسجيل الدخول ل * [ " ${client.user.username} " ]`);
+console.log('')
+console.log('معلومات : ')
+console.log('')
+console.log(`عدد السيرفرات♤ [ " ${client.guilds.size} " ]`);
+console.log(`المستخدمين♡ [ " ${client.users.size} " ]`);
+console.log(`الرومات♧ [ " ${client.channels.size} " ]`);
+console.log('╚[════════════════════════════════════]╝')
+console.log('By Jackeo')
+console.log('╔[════════════]╗')
+console.log(' البوت اون لاين')
+console.log('╚[════════════]╝')
+console.log('V2 العربية arap ')
+console.log('Dev Narox by Jackeo')
+
+
+//النسخة المعربة من جاكيو 
+
+
+});
+
+//حط الاكواد تحت ↓↓↓
+
+
+
+
+client.login(NTQxMjU5MTIxNjkzOTQ5OTY1.Dz88SA.wWNnXtg9BpeurCSm1zVu6PKzlWw);
